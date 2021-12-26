2146115fbb0c6853aecdfc3a098501b88442d984
Dvir Borochov <dvir.borochov@gmail.com>
2021-12-26 18:49:28 +0200

    last sun

--- graph.c
+++ graph.c
@@ -2,25 +2,49 @@
 #include <stdlib.h>
 #include "graph.h"
 
-pnode build_graph_cmd(int nodeSum){
-    int curr;
+pnode build_graph_cmd(int nodeSum)
+{
+    char Ccurr;
+    int Icurr;
     int first = 1;
-  while (nodeSum)
-  {
-    if (!scanf(" %d", &curr)) //if  char == (n)
+    pnode prev;
+    while (nodeSum)
     {
-        if(first){
+        if (scanf(" %c", &Ccurr)) // if  char == (n)
+        {
+            while (scanf(" %d", &Icurr)) // while the input isn't n:
+            {
+                if (first)
+                { // define the head node:
+                    node head = malloc(sizeof(node));
+                    pnode phead = &head;
+                    prev = &head;
+                    head.node_num = Icurr;
+                    head.next = NULL;
+                    first = 0;
+                }
+                else
+                {
+                 node n = malloc(sizeof(node));
+                 pnode pn = &n;
+                 n.node_num = Icurr; //need to connect the frev
+                 n.next = prev;
+                 prev = pn;
+                }
+                
+                    int dest = Icurr;
+                    scanf(" %d", &Icurr);
+                    int weight = Icurr;
+                    edge e = malloc(sizeof(edge));
+                    e.weight = weight;
+                    //check if the dest node already exist - need to build a function..
+                    e.endpoint = dest;
+                
+            }
 
-
-            first = 0;
+            //  scanf(" %d", &curr);
         }
-        scanf(" %d", &curr);
-
+        nodeSum--;
     }
-    nodeSum--;
-  }
- 
+    return phead;
 }
- //  node head = malloc(sizeof(node));
-         //   pnode phead = &head;
-          //  a.start = phead;
